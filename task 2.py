2d1e31d358eb414a70181f03e118839bdedb2074
7Stanislav <112968442+7Stanislav@users.noreply.github.com>
2023-02-05 12:10:13 +0200

    Completed task 2

--- task 2.py
+++ task 2.py
@@ -11,3 +11,26 @@ length (длина в метрах), width (ширина в метрах).
 Проверить работу метода.
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
+
+class Road:
+    __length = 0
+    __width = 0
+    weight = 0
+    thickness = 0
+
+    def mass(self, __length, __width, weight, thickness):
+        self.__length = __length
+        self.__width = __width
+        self.weight = weight
+        self.thickness = thickness
+        total_mass = __length * __width * weight * thickness
+        total_mass_t = total_mass / 1000
+        print(f"{__length}м*{__width}м*{weight}кг*{thickness}м = {int(total_mass)} кг = {total_mass_t} т")
+
+a = Road()
+
+__length = int(input("Введите длину в метрах: "))
+__width = int(input("Введите ширину в метрах: "))
+weight = int(input("Введите массу асфальта в кг: "))
+thickness = float(input("Введите толшину асфальта в м: "))
+a.mass(__length, __width, weight, thickness)
